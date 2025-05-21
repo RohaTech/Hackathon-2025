1997fa127336f03d0c81d7854de00263aa270042
voidrak <raklovetocode@gmail.com>
2025-05-22 01:31:26 +0300

    update

--- Client/src/views/Head Finance/HeadFinancePayrollHistory.vue
+++ Client/src/views/Head Finance/HeadFinancePayrollHistory.vue
@@ -1,7 +1,50 @@
 <script setup>
 import HeadFinanceLayout from "@/layout/HeadFinanceLayout.vue";
+import { useEmployeeStore } from "@/stores/employee";
+
+const { getEmployees } = useEmployeeStore();
+// const employees
+
+onMounted(async () => {
+  getEmployees;
+});
+
+const excelFields = {
+  Name: "name",
+  Username: "userName",
+  Date_of_birth: "birthDay",
+  Phone_number: "phoneNumber",
+  Address: "address",
+  Facebook_address: "facebook",
+};
+
+const dataExcel = [
+  {
+    name: "Phạm Thế Chiêm",
+    userName: "chiempt",
+    birthDay: "27/09/1999",
+    phoneNumber: "0345361887",
+    address: "Hoài Đức - Hà Nội",
+    facebook: "https://www.facebook.com/chiem.dieu.18/",
+  },
+];
 </script>
 
 <template>
-  <HeadFinanceLayout> HeadFinancePayrollHistory </HeadFinanceLayout>
+  <HeadFinanceLayout>
+    <p class="">HeadFinancePayrollHistory</p>
+
+    <export-excel
+      :data="dataExcel"
+      :fields="excelFields"
+      :title="'Employee Salary Sheet For Ginbot'"
+      :name="`employee_payroll_for_ginbot.xls`"
+    >
+      <button
+        class="flex mt-10 items-center w-fit justify-center rounded-lg bg-[#0a5098] px-4 py-3 text-sm font-medium text-white shadow-theme-xs transition hover:bg-[#f3a21b]"
+      >
+        Download Excel
+      </button>
+    </export-excel>
+  </HeadFinanceLayout>
 </template>
