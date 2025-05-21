c626b4f225678d8d9766c496deb866880379db7c
Dawithabte31 <dawithabitamu31@gmail.com>
2025-05-21 13:31:40 +0300

    `Added dependencies and routes for exporting users`

--- Server/routes/api.php
+++ Server/routes/api.php
@@ -3,6 +3,9 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UsersController;
+
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -13,3 +16,5 @@ Route::get('/user', function (Request $request) {
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+Route::get('/users/export', [UsersController::class, 'export']);
+
