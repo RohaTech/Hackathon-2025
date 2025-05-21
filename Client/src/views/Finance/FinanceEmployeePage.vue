fcbe50916b6b46ea014aea1d3cfdd60ab90f2c1d
voidrak <raklovetocode@gmail.com>
2025-05-21 19:21:13 +0300

    remove doctors

--- Client/src/views/Finance/FinanceEmployeePage.vue
+++ Client/src/views/Finance/FinanceEmployeePage.vue
@@ -18,7 +18,6 @@ const showPopup = ref(false);
 const selectedEmployee = ref(null);
 
 const openDetailPopup = (employee) => {
-  
   selectedEmployee.value = employee;
   console.log(selectedEmployee.value);
   showPopup.value = true;
@@ -41,14 +40,14 @@ const rename = (value) => {
 
 const showAddEmployeeModal = ref(false);
 const newEmployee = ref({
-  name: '',
-  gender: '',
-  email: '',
-  employement_type: 'full_time',
-  position: '',
-  employement_date: '',
-  basic_salary: '',
-  account_number: '',
+  name: "",
+  gender: "",
+  email: "",
+  employement_type: "full_time",
+  position: "",
+  employement_date: "",
+  basic_salary: "",
+  account_number: "",
   balance: 0,
 });
 
@@ -59,14 +58,14 @@ const closeAddEmployeeModal = () => {
   showAddEmployeeModal.value = false;
   // Reset form fields
   newEmployee.value = {
-    name: '',
-    email: '',
-    gender: '',
-    employement_type: 'full_time',
-    position: '',
-    employement_date: '',
-    basic_salary: '',
-    account_number: '',
+    name: "",
+    email: "",
+    gender: "",
+    employement_type: "full_time",
+    position: "",
+    employement_date: "",
+    basic_salary: "",
+    account_number: "",
     balance: 0,
   };
 };
@@ -79,8 +78,6 @@ const submitAddEmployee = async () => {
   employees.value = await getEmployees();
   closeAddEmployeeModal();
 };
-
-
 </script>
 
 <template>
@@ -96,13 +93,17 @@ const submitAddEmployee = async () => {
         <thead>
           <tr class="ark:border-gray-700 border-b border-gray-200">
             <th class="w-2/11 px-5 py-3 text-left sm:px-6">
-              <p class="text-theme-xs font-medium text-gray-500">Employee Name</p>
+              <p class="text-theme-xs font-medium text-gray-500">
+                Employee Name
+              </p>
             </th>
             <th class="w-2/11 px-5 py-3 text-left sm:px-6">
               <p class="text-theme-xs font-medium text-gray-500">Email</p>
             </th>
             <th class="w-2/11 px-5 py-3 text-left sm:px-6">
-              <p class="text-theme-xs font-medium text-gray-500">Employement Type</p>
+              <p class="text-theme-xs font-medium text-gray-500">
+                Employement Type
+              </p>
             </th>
             <th class="w-2/11 px-5 py-3 text-left sm:px-6">
               <p class="text-theme-xs font-medium text-gray-500">position</p>
@@ -111,17 +112,19 @@ const submitAddEmployee = async () => {
               <p class="text-theme-xs font-medium text-gray-500">Employement Date</p>
             </th> -->
             <th class="w-2/15 px-5 py-3 text-left sm:px-6">
-              <p class="text-theme-xs font-medium text-gray-500">Basic Salary</p>
+              <p class="text-theme-xs font-medium text-gray-500">
+                Basic Salary
+              </p>
             </th>
             <!-- <th class="w-2/11 px-5 py-3 text-left sm:px-6">
               <p class="text-theme-xs font-medium text-gray-500">Employement Date</p>
             </th> -->
             <th class="w-2/11 px-5 py-3 text-left sm:px-6">
-              <p class="text-theme-xs font-medium text-gray-500">Actions </p>
+              <p class="text-theme-xs font-medium text-gray-500">Actions</p>
             </th>
           </tr>
         </thead>
-      <tbody class="ark:divide-gray-700 divide-y divide-gray-200">
+        <tbody class="ark:divide-gray-700 divide-y divide-gray-200">
           <!-- No results message -->
           <tr v-if="employees.length === 0">
             <td colspan="7" class="px-5 py-8 text-center sm:px-6">
@@ -141,7 +144,7 @@ const submitAddEmployee = async () => {
                   />
                 </svg>
                 <p class="mt-2 text-base text-gray-500">
-                  No doctors found with status:
+                  No Employees found with status:
                   <span class="font-medium capitalize">{{
                     selectedStatus
                   }}</span>
@@ -150,7 +153,7 @@ const submitAddEmployee = async () => {
                   @click="changeStatus('all')"
                   class="mt-4 rounded-lg border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
                 >
-                  Show All Doctors
+                  Show All Employees
                 </button>
               </div>
             </td>
@@ -191,11 +194,7 @@ const submitAddEmployee = async () => {
             </td> -->
             <td class="px-5 py-4 sm:px-6">
               <p class="text-theme-sm text-gray-500">
-                <span
-                  
-                >
-                  {{ employee.basic_salary }}</span
-                >
+                <span> {{ employee.basic_salary }}</span>
               </p>
             </td>
             <td class="px-5 py-4 sm:px-6">
@@ -210,7 +209,7 @@ const submitAddEmployee = async () => {
         </tbody>
       </table>
     </div>
-  <Modal v-if="showPopup" @close="closePopup" :fullScreenBackdrop="true">
+    <Modal v-if="showPopup" @close="closePopup" :fullScreenBackdrop="true">
       <template #body>
         <div
           class="relative max-h-[700px] w-full max-w-[700px] overflow-y-auto overflow-x-hidden rounded-3xl bg-white p-4 lg:p-11"
@@ -249,7 +248,7 @@ const submitAddEmployee = async () => {
                     Employement Type
                   </label>
                   <p class="text-base text-gray-800">
-                    {{ rename(selectedEmployee.employement_type)}} 
+                    {{ rename(selectedEmployee.employement_type) }}
                   </p>
                 </div>
 
@@ -277,7 +276,9 @@ const submitAddEmployee = async () => {
                     Account Number
                   </label>
                   <p class="text-base text-gray-800">
-                    {{ selectedEmployee.account.account_number || "Not provided" }}
+                    {{
+                      selectedEmployee.account.account_number || "Not provided"
+                    }}
                   </p>
                 </div>
 
@@ -302,7 +303,6 @@ const submitAddEmployee = async () => {
             </div>
 
             <!-- Location and License Info Section -->
-            
 
             <!-- License Dates and Status -->
             <div class="mt-6">
@@ -362,7 +362,6 @@ const submitAddEmployee = async () => {
                   {{ selectedDoctor.status }}
                 </span>
               </div> -->
-            
             </div>
 
             <!-- Action buttons -->
@@ -381,33 +380,60 @@ const submitAddEmployee = async () => {
       </template>
     </Modal>
 
-    <Modal v-if="showAddEmployeeModal" @close="closeAddEmployeeModal" :fullScreenBackdrop="true">
+    <Modal
+      v-if="showAddEmployeeModal"
+      @close="closeAddEmployeeModal"
+      :fullScreenBackdrop="true"
+    >
       <template #body>
-        <div class="relative max-h-[700px] w-full max-w-[700px] overflow-y-auto overflow-x-hidden rounded-3xl bg-white p-4 lg:p-11">
+        <div
+          class="relative max-h-[700px] w-full max-w-[700px] overflow-y-auto overflow-x-hidden rounded-3xl bg-white p-4 lg:p-11"
+        >
           <div class="flex justify-between items-center mb-6">
             <h2 class="text-2xl font-bold">Add Employee</h2>
-            <button @click="closeAddEmployeeModal" class="text-gray-500 hover:text-gray-700 text-xl">&times;</button>
+            <button
+              @click="closeAddEmployeeModal"
+              class="text-gray-500 hover:text-gray-700 text-xl"
+            >
+              &times;
+            </button>
           </div>
           <form @submit.prevent="submitAddEmployee">
             <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
               <div>
                 <label class="block mb-1 font-medium">Name</label>
-                <input v-model="newEmployee.name" type="text" required class="w-full border rounded px-3 py-2" />
+                <input
+                  v-model="newEmployee.name"
+                  type="text"
+                  required
+                  class="w-full border rounded px-3 py-2"
+                />
               </div>
               <div>
                 <label class="block mb-1 font-medium">Email</label>
-                <input v-model="newEmployee.email" type="email" required class="w-full border rounded px-3 py-2" />
+                <input
+                  v-model="newEmployee.email"
+                  type="email"
+                  required
+                  class="w-full border rounded px-3 py-2"
+                />
               </div>
               <div>
                 <label class="block mb-1 font-medium">Employment Type</label>
-                <select v-model="newEmployee.employement_type" class="w-full border rounded px-3 py-2">
+                <select
+                  v-model="newEmployee.employement_type"
+                  class="w-full border rounded px-3 py-2"
+                >
                   <option value="full_time">Full Time</option>
                   <option value="part_time">Part Time</option>
                 </select>
               </div>
               <div>
                 <label class="block mb-1 font-medium">Employment Type</label>
-                <select v-model="newEmployee.position" class="w-full border rounded px-3 py-2">
+                <select
+                  v-model="newEmployee.position"
+                  class="w-full border rounded px-3 py-2"
+                >
                   <option value="ceo">CEO</option>
                   <option value="coo">COO</option>
                   <option value="cto">CTO</option>
@@ -426,7 +452,7 @@ const submitAddEmployee = async () => {
                   for="birth_date"
                   class="mb-1 block text-sm text-[#0F172A]"
                 >
-                 Employment Date<span class="text-red-500">*</span>
+                  Employment Date<span class="text-red-500">*</span>
                 </label>
                 <flat-pickr
                   v-model="newEmployee.employement_date"
@@ -442,28 +468,53 @@ const submitAddEmployee = async () => {
               </div> -->
               <div>
                 <label class="block mb-1 font-medium">Basic Salary</label>
-                <input v-model="newEmployee.basic_salary" type="number" required class="w-full border rounded px-3 py-2" />
+                <input
+                  v-model="newEmployee.basic_salary"
+                  type="number"
+                  required
+                  class="w-full border rounded px-3 py-2"
+                />
               </div>
               <div class="">
                 <label class="block mb-1 font-medium">Account Number</label>
-                <input v-model="newEmployee.account_number" type="text" required class="w-full border rounded px-3 py-2" />
+                <input
+                  v-model="newEmployee.account_number"
+                  type="text"
+                  required
+                  class="w-full border rounded px-3 py-2"
+                />
               </div>
               <div>
                 <label class="block mb-1 font-medium">Gender</label>
-                <select v-model="newEmployee.gender" class="w-full border rounded px-3 py-2">
+                <select
+                  v-model="newEmployee.gender"
+                  class="w-full border rounded px-3 py-2"
+                >
                   <option value="male">Male</option>
                   <option value="female">Female</option>
                 </select>
               </div>
             </div>
-            <div class="modal-footer mt-8 flex items-center gap-3 sm:justify-end">
-              <button type="button" @click="closeAddEmployeeModal" class="ark:border-gray-700 ark:bg-gray-800 ark:hover:bg-white/[0.03] flex w-full justify-center rounded-lg border border-gray-300 bg-white px-4 py-2.5 text-sm font-medium text-gray-700 hover:bg-gray-50 sm:w-auto">Close</button>
-              <button type="submit" class="flex w-full justify-center rounded-lg border border-[#0a5098] bg-[#121fb5] px-4 py-2.5 text-sm font-medium text-white hover:bg-[#0a1698] sm:w-auto">Submit</button>
+            <div
+              class="modal-footer mt-8 flex items-center gap-3 sm:justify-end"
+            >
+              <button
+                type="button"
+                @click="closeAddEmployeeModal"
+                class="ark:border-gray-700 ark:bg-gray-800 ark:hover:bg-white/[0.03] flex w-full justify-center rounded-lg border border-gray-300 bg-white px-4 py-2.5 text-sm font-medium text-gray-700 hover:bg-gray-50 sm:w-auto"
+              >
+                Close
+              </button>
+              <button
+                type="submit"
+                class="flex w-full justify-center rounded-lg border border-[#0a5098] bg-[#121fb5] px-4 py-2.5 text-sm font-medium text-white hover:bg-[#0a1698] sm:w-auto"
+              >
+                Submit
+              </button>
             </div>
           </form>
         </div>
       </template>
     </Modal>
-
   </FinanceLayout>
 </template>
